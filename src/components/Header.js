8b6479d08fc21507051e2bf1974c32749cd8a0bd
shriya <shriyachandel1998@gmail.com>
2025-10-27 12:52:20 +0530

    Update in contact css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
 import "./css/Header.css";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  // 👉 Sticky Header effect on scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector(".header");
+      if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <header className="header">
       <div className="logo">
         <img src="/shrii.jpeg" alt="Shriya Chandel" />
-       
-        </div>
+      </div>
 
       <nav className={`nav ${menuOpen ? "open" : ""}`}>
         <ul>
-           <li><Link to="about" smooth={true} duration={500}>About</Link></li>
-           <li><Link to="projects" smooth={true} duration={800}>Projects</Link></li>
-           <li><Link to="contact" smooth={true} duration={800}>Contact</Link></li>
+          <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+          <li><Link to="projects" smooth={true} duration={800}>Projects</Link></li>
+          <li><Link to="contact" smooth={true} duration={800}>Contact</Link></li>
         </ul>
       </nav>
 
